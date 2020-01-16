68935aa7c88e9aac92bf30ad509e95dba03e7ab3
aaharraz <aaharraz@yahoo.com>
2020-01-15 23:08:41 -0700

    Test Module

---
+++ NasserHarraz_vog_backend_codechalleneg/ModuleTest/UnitTest.cs
@@ -0,0 +1,63 @@
+using System;
+using Xunit;
+using VogCodeChallenge.API.Classes;
+
+namespace ModuleTest
+{
+    public class UnitTest
+    {
+        [Fact]
+        public void ModuleInt1ReturnDoubler()
+        {
+            int expected = 2;
+            object actual = TestModule.Module(1);
+            Assert.Equal(expected, actual);
+       
+        }
+
+        [Fact]
+        public void ModuleInt2ReturnDoubler()
+        {
+            int expected = 4;
+            object actual = TestModule.Module(2);
+            Assert.Equal(expected, actual);
+
+        }
+
+        [Fact]
+        public void ModuleInt3ReturnTrippler()
+        {
+            int expected = 9;
+            object actual = TestModule.Module(3);
+            Assert.Equal(expected, actual);
+
+        }
+
+        [Fact]
+        public void ModuleIntGreaterThan3ReturnTrippler()
+        {
+            int expected = 15;
+            object actual = TestModule.Module(5);
+            Assert.Equal(expected, actual);
+
+        }
+
+        [Fact]
+        public void ModuleFloatValueReturnLeveler()
+        {
+            double expected = (0.0);
+            object actual = TestModule.Module(1.0f);
+            Assert.Equal(expected, actual);
+
+        }
+
+        [Fact]
+        public void ModuleAnyOtherValueReturnTheSameValue()
+        {
+            double expected = (0.5);
+            object actual = TestModule.Module(0.5);
+            Assert.Equal(expected, actual);
+
+        }
+    }
+}
---
+++ NasserHarraz_vog_backend_codechalleneg/VogCodeChallenge.API/Classes/TestModule.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace VogCodeChallenge.API.Classes
+{
+    public class TestModule
+    {
+        public static object Module(object testObject)
+        {
+            switch (testObject)
+            {
+                case 1:
+                case 2:
+                    return doubler((int)testObject);
+                case int value when value >= 3:
+                    return tripler(value);
+                case float floatValue when floatValue == 1.0f:
+                    return leveler(floatValue);
+                default:
+                    return testObject;
+            }
+
+
+            int doubler(int amount) => amount * 2;
+            int tripler(int amount) => amount * 3;
+            // bug fixed amount type changed to float
+            double leveler(float amount) => amount - 0.1f * 10f;
+        }
+    }
+}
